de2a505c58b4e6dbee0b9ef72423dd3815a9d4f2
Mundanesleet <jalex17nov@outlook.com>
2025-04-05 12:09:46 -0500

    Actalizacion comentarios git

---
+++ main.py
@@ -0,0 +1,12 @@
+# Comando para crear rama
+'git branch nombre-de-la-rama'
+
+#Para ver ramas existentes y en la cual se encuentran
+'git branch'
+
+#Crear una rama y pasar a ella directamente
+'git checkout -b nombre-de-la-rama'
+
+#Para cambiar entre ramas
+'git checkout nombre-de-rama-existente'
+
